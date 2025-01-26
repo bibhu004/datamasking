6146dc3d9580f22599f631f93d294999e048b473
bibhup04 <bibhusunder.b@prodapt.com>
2025-01-26 19:59:28 +0530

    log using lombok fixd

--- src/main/java/com/masking/datamasking/Custom/LoggingAspect.java
+++ src/main/java/com/masking/datamasking/Custom/LoggingAspect.java
@@ -6,34 +6,37 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.masking.datamasking.Dto.TransferRequestDto;
 import com.masking.datamasking.Service.TransferService;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
 
     // @Autowired
     // private TransferService transferService;
 
-    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+    // private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
 
     
     @Before("execution(* com.masking.datamasking.Controller.*.*(..))")
     public void logBefore(){
-        logger.info("\nBefore Method execution.\n");
+        log.info("\nBefore Method execution.\n");
     }
 
     @Before("execution(* com.masking.datamasking.Controller.*.authenticateAndGetToken(..))")
     public void logLoginBefore(){
-        logger.info("\nBefore login Method execution.\n");
+        log.info("\nBefore login Method execution.\n");
     }
 
     @After("execution(* com.masking.datamasking.Controller.*.*(..))")
@@ -48,30 +51,30 @@ public class LoggingAspect {
 
     @Before("execution(* com.masking.datamasking.Service.TransferService.transferMoney(..))")
     public void logTransferService(){
-        logger.info("inside transfer");
+        log.info("inside transfer");
     }
 
 
     @Around("execution(* com.masking.datamasking.Service.TransferService.transferMoney(..))")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        logger.info("Before executing transferMoney method...");
+        log.info("Before executing transferMoney method...");
         // System.out.println("Before executing transferMoney method...");
         // Log the input arguments (TransferRequestDto in this case)
         Object[] args = joinPoint.getArgs();
         if (args.length > 0 && args[0] instanceof TransferRequestDto) {
             TransferRequestDto dto = (TransferRequestDto) args[0];
-            logger.info("Input Details: ");
-            logger.warn("From Account: {}", dto.getFromAccount());
-            logger.debug("To Account: {}", dto.getToAccount());
-            logger.error("Amount: ${}", dto.getAmount());
+            log.info("Input Details: ");
+            log.warn("From Account: {}", dto.getFromAccount());
+            log.debug("To Account: {}", dto.getToAccount());
+            log.error("Amount: ${}", dto.getAmount());
         }
 
         // Proceed with the method execution
         Object result = joinPoint.proceed();
 
         // Log the result
-        logger.info("After executing transferMoney method...");
-        logger.info("Result: {}", result);
+        log.info("After executing transferMoney method...");
+        log.info("Result: {}", result);
 
         return result;
     }
